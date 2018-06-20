fb09d80b986e72fc8cbbca8022616f8251f98a3c
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-06-20 00:27:37 -0400

    changes

--- custom.js
+++ custom.js
@@ -1,16 +1,16 @@
-// window.MathJax = {
-//     tex2jax: {
-//         inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
-//         displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-//         processEscapes: true
-//     },
-//     TeX: { 
-//         equationNumbers: { autoNumber: "AMS" },
-//         extensions: ["autoload-all.js"],
-//         Macros: {
-//             diff: ["{\\mathrm{d}#1}",1],
-//             integral: ["{\\int{#1\\,\\mathrm{d}#2}}", 2],
-//             defint: ["{\\int\\limits_{#1}^{#2}{#3\\,\\mathrm{d}#4}}", 4]
-//         }
-//     }
-// };
+window.MathJax = {
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        processEscapes: true
+    },
+    TeX: { 
+        equationNumbers: { autoNumber: "AMS" },
+        extensions: ["autoload-all.js"],
+        Macros: {
+            diff: ["{\\mathrm{d}#1}",1],
+            integral: ["{\\int{#1\\,\\mathrm{d}#2}}", 2],
+            defint: ["{\\int\\limits_{#1}^{#2}{#3\\,\\mathrm{d}#4}}", 4]
+        }
+    }
+};
