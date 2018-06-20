b791e832955a5bde894b9683e91c0d6e2d995d6c
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-06-20 00:19:26 -0400

    changes

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ window.MathJax = {
         equationNumbers: { autoNumber: "AMS" },
         extensions: ["autoload-all.js"],
         Macros: {
-            diff: ["{\\mathrm{d}#1}",1]
+            diff: ["{\\mathrm{d}#1}",1],
             integral: ["{\\int{#1\\,\\mathrm{d}#2}}", 2],
             defint: ["{\\int\\limits_{#1}^{#2}{#3\\,\\mathrm{d}#4}}", 4]
         }
