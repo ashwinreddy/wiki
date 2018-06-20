b1089c35b3d75cbee21172008737a94907ea16b7
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-06-19 23:58:11 -0400

    changes

--- config.js
+++
@@ -1,8 +0,0 @@
-MathJax.Hub.Config({
-    tex2jax: {
-        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
-        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-        processEscapes: true
-      },
-      TeX: { extensions: ["autoload-all.js"] }
-});
---
+++ custom.js
@@ -0,0 +1,8 @@
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        processEscapes: true
+      },
+      TeX: { extensions: ["autoload-all.js"] }
+});
