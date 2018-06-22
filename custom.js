b2d99902fa43d5bfd99b6db82f7aa48a5053e03e
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-06-22 11:21:31 -0400

    changes

--- custom.js
+++ custom.js
@@ -11,7 +11,8 @@ window.MathJax = {
             diff: ["{\\mathrm{d}#1}",1],
             integral: ["{\\int{#1\\,\\mathrm{d}#2}}", 2],
             defint: ["{\\int\\limits_{#1}^{#2}{#3\\,\\mathrm{d}#4}}", 4],
-            deriv: ["\\frac{\\diff{#1}}{\\diff{#2}}", 2]
+            deriv: ["\\frac{\\diff{#1}}{\\diff{#2}}", 2],
+            set: ["\\{#1 : #2 \\}", 2]
         }
     }
 };
