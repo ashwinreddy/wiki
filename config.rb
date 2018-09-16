fb0ed9b4873ab790d694adad7652ca9a8ecd05a4
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-09-16 15:27:19 -0700

    add config just to be safe

--- config.rb
+++ config.rb
@@ -3,3 +3,7 @@ Precious::App.set(:wiki_options, {
     :live_preview => true,
     :allow_uploads => true
 })
+
+GitHub::Markup::Markdown::MARKDOWN_GEMS['kramdown'] = proc { |content|
+  Kramdown::Document.new(content, :auto_ids => false).to_html
+}
