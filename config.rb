41ea62146e6d318128048c2fd743d9676306090d
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-09-22 13:24:47 -0700

    changes

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ Precious::App.set(:wiki_options, {
     :universal_toc => true ,
     :live_preview => true,
     :allow_uploads => true,
-    :plantuml_url => "https://www.plantuml.com/plantuml/png"
+    :plantuml_url => "http://www.plantuml.com/plantuml/png"
 })
 
 GitHub::Markup::Markdown::MARKDOWN_GEMS['kramdown'] = proc { |content|
