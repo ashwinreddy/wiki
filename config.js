e5c83d4dea5a4b19108fb9691cb21eb15b0ce189
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-09-30 19:16:18 -0700

    changes to js

---
+++ config.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        processEscapes: true
+    },
+    TeX: { 
+        equationNumbers: { autoNumber: "AMS" },
+        extensions: ["autoload-all.js", "autobold.js"]
+    }
+};
+
+MathJax.Hub.Config({
+    TeX: {
+      Macros: {
+        RR: '{\\bf R}',
+        defeq: ['{\\mathrel{\\unicode{x2254}}}']
+      }
+    }
+  });
