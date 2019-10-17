87cf23f4619c99d7a37f79de16b7388e81dc3d74
Ashwin Reddy <adreddy@berkeley.edu>
2019-10-16 17:54:14 -0700

    Doubled up on slashes as escapes

--- mathjax.config.js
+++ mathjax.config.js
@@ -8,10 +8,10 @@ window.MathJax = {
         equationNumbers: { autoNumber: "AMS" },
         extensions: ["autoload-all.js", "autobold.js"],
         Macros: {
-            RR: '{\mathbb{R}}',
-            dd: '{\mathrm{d}#1}',
-            dv: '\frac{\dd{#1}}{\dd{#2}}',
-            order: '{\mathcal{O}\left(#1\right)}',
+            RR: '{\\mathbb{R}}',
+            dd: '{\\mathrm{d}#1}',
+            dv: '{\\frac{\\dd{#1}}{\\dd{#2}}}',
+            order: '{\\mathcal{O}\left(#1\right)}',
             defeq: ['{\\mathrel{\\unicode{x2254}}}']
       }
     }
