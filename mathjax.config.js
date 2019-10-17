3d515c589c2742405925ae92d3ed7af357d7e059
Ashwin Reddy <adreddy@berkeley.edu>
2019-10-16 17:58:53 -0700

    Doubled up on slashes as escapes for left and right commands

--- mathjax.config.js
+++ mathjax.config.js
@@ -11,7 +11,7 @@ window.MathJax = {
             RR: '{\\mathbb{R}}',
             dd: ['{\\mathrm{d}#1}', 1],
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
-            order: ['{\\mathcal{O}\left(#1\right)}', 1],
+            order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
       }
     }
 };
