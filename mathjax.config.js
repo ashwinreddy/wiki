594a1f589ac9f5bf929647aa238e344878defbda
Ashwin Reddy <adreddy@berkeley.edu>
2019-10-16 17:57:19 -0700

    Included argument numbers for macros

--- mathjax.config.js
+++ mathjax.config.js
@@ -9,10 +9,9 @@ window.MathJax = {
         extensions: ["autoload-all.js", "autobold.js"],
         Macros: {
             RR: '{\\mathbb{R}}',
-            dd: '{\\mathrm{d}#1}',
-            dv: '{\\frac{\\dd{#1}}{\\dd{#2}}}',
-            order: '{\\mathcal{O}\left(#1\right)}',
-            defeq: ['{\\mathrel{\\unicode{x2254}}}']
+            dd: ['{\\mathrm{d}#1}', 1],
+            dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
+            order: ['{\\mathcal{O}\left(#1\right)}', 1],
       }
     }
 };
