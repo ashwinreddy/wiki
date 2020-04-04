2db267adbd277d61a2cae03d79da81a1876ff790
Ashwin Reddy <adreddy@berkeley.edu>
2020-04-04 02:45:45 -0700

    pdv

--- custom.js
+++ custom.js
@@ -11,6 +11,7 @@ window.MathJax = {
             RR: '{\\mathbb{R}}',
             dd: ['{\\mathrm{d}#1}', 1],
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
+ 	    pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
             order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
       }
     },
