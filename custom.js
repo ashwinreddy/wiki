35f28f659b3d9898f678a260f450a3670779a488
Ashwin Reddy <adreddy@berkeley.edu>
2020-04-04 02:43:39 -0700

    mod

--- custom.js
+++ custom.js
@@ -1,4 +1,19 @@
 window.MathJax = {
-  loader: {load: ['[tex]/physics']},
-  tex: {packages: {'[+]': ['physics']}}
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        processEscapes: true
+    },
+    TeX: { 
+        equationNumbers: { autoNumber: "AMS" },
+        extensions: ["autoload-all.js", "autobold.js"],
+        Macros: {
+            RR: '{\\mathbb{R}}',
+            dd: ['{\\mathrm{d}#1}', 1],
+            dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
+            order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
+      }
+    },
+    loader: {load: ['[tex]/physics']},
+    tex: {packages: {'[+]': ['physics']}}
 };
