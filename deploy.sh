1c3ea0202080941e8c0cdb7351e3d04436569a0d
Ashwin Reddy <adreddy@berkeley.edu>
2020-06-03 02:35:37 -0700

    nest group inside abstract

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gollum --mathjax --critic-markup --config config.rb --js --lenient-tag-lookup --emoji ./
+gollum --mathjax --critic-markup --config config.rb --js --lenient-tag-lookup --emoji --allow-uploads dir ./
