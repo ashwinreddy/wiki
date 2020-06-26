1c0590d10b4c5bb7614d62cb02992c080e988285
Ashwin Reddy <adreddy@berkeley.edu>
2020-06-26 04:41:19 -0700

    add transpose command

--- custom.js
+++ custom.js
@@ -11,8 +11,9 @@ window.MathJax = {
             RR: '{\\mathbb{R}}',
             dd: ['{\\mathrm{d}#1}', 1],
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
- 	    pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
+ 	        pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
             order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
+            transpose: ['{#1^\\intercal}', 1]
       }
     },
     loader: {load: ['[tex]/physics']},
