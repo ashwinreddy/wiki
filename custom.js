b463d068468c1ab84f6b4a3f55cba9d9771f68e0
Ashwin Reddy <adreddy@berkeley.edu>
2020-06-26 04:53:43 -0700

    relabel transpose

--- custom.js
+++ custom.js
@@ -13,7 +13,7 @@ window.MathJax = {
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
  	        pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
             order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
-            transpose: ['{\\mathcal{O}\\left(#1\\right)}', 1],
+            tpose: ['{#1^\\intercal}', 1]
       }
     },
     loader: {load: ['[tex]/physics']},
