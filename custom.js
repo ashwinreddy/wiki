6c88bec7667be423dcfd93fa87292c3f2e2acf4f
Ashwin Reddy <adreddy@berkeley.edu>
2020-06-28 02:27:53 -0700

    tpose

--- custom.js
+++ custom.js
@@ -9,7 +9,6 @@ window.MathJax = {
         extensions: ["autoload-all.js", "autobold.js"],
         Macros: {
             RR: '{\\mathbb{R}}',
-            tpose: ['{#1^\\intercal}', 1],
             dd: ['{\\mathrm{d}#1}', 1],
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
  	        pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
@@ -26,7 +25,8 @@ MathJax.Hub.Config({
 	TeX: {
 		Macros: {
 
-	    eqdef: '{\,\\doteq\,}'
+	    eqdef: '{\,\\doteq\,}',
+            tpose: ['{#1^\\intercal}', 1],
 		}
 	}
 })
