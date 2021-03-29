9e369a3677b466aaec10ff413b4a1e6d090ab07b
Ashwin Reddy <adreddy@berkeley.edu>
2021-03-28 22:29:33 -0700

    fix custom code

--- custom.js
+++ custom.js
@@ -12,8 +12,9 @@ window.MathJax = {
             dd: ['{\\mathrm{d}#1}', 1],
             dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
  	        pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
-	   expectation: ['{\\mathbb{E}\\left[#1\\right]}', 1 ],
+	   expectation: ['{\\mathbb{E}\\left(#1\\right)}', 1 ],
             order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
+	   im: ['{\\iota}']
       }
     },
     loader: {load: ['[tex]/physics']},
