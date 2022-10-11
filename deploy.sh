a12b2d86d73e5bfb7d70e5e752421613fd0a53a7
Ashwin Reddy <adreddy@berkeley.edu>
2022-10-10 17:46:57 -0700

    add config

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gollum --mathjax --critic-markup --config config.rb --js --lenient-tag-lookup --emoji --allow-uploads dir ./ --css --no-display-metadata
-#gollum --mathjax --no-display-metadata --lenient-tag-lookup --config config.rb
+
+gollum --mathjax --lenient-tag-lookup --no-display-metadata --allow-uploads 
--- mathjax.config.js
+++ mathjax.config.js
@@ -1,10 +1,22 @@
 window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]
-  },
-  svg: {
-    fontCache: 'global'
-  }
+    tex2jax: {
+        inlineMath:  [ ['$','$'], ['\\(','\\)'] ],
+        displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+        //processEscapes: true
+    },
+    TeX: { 
+        Macros: {
+            RR: '{\\mathbf{R}}',
+	     dd: ['{\\mathrm{d}#1}', 1],
+	     dv: ['{\\frac{\\dd{#1}}{\\dd{#2}}}', 2],
+	   pdv: ['{\\frac{\\partial{#1}}{\\partial{#2}}}', 2],
+	   expectation: ['{\\mathbb{E}\\left(#1\\right)}', 1 ],
+           order: ['{\\mathcal{O}\\left(#1\\right)}', 1],
+	   im: ['{\\iota}']
+      }
+    },
+    loader: { load: ['[tex]/cancel'] },
+    tex: { packages: { '[+]': ['cancel'] }}
 };
 
-console.log("new mathjax.js");
+
